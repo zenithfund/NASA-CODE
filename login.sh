f9498f7d0ad2c5263995b87b19085a2d88a06e8c
ike Michael <ikendichie76@gmail.com>
2022-02-19 22:31:28 +0000

    my first commit

---
+++ login.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#this script is for authenticate users identification
+
+echo "please enter your name"
+read name
+echo "please enter your password"
+read -s password
+echo "you have succesfully login"
